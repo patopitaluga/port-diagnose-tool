3083ce7db7b3a83d6471a9a64dbf6c800e2a4f94
Patricio Pitaluga <patricio.pitaluga@gmail.com>
2022-11-16 15:14:22 -0300

    Update

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const httpServer = require('http');
+const dgram = require('node:dgram');
 
-const ports = [
-  53, // dns
+const tcpPorts = [
   80, // http
   82, // ngrok
   83, // ngrok
-  123, // ntp
   443, // https
   1883, // emqtt
   4443, // ngrok tunneling
@@ -15,10 +14,50 @@ const ports = [
   9000, // firmware upgrade
 ];
 
-console.log(`Server listening ports: ${ports.join(',')}`)
-ports.forEach((port) => {
-  const server = httpServer.createServer((req, res) => res.end('1')).listen(port);
-  server.on('error', function (e) {
-    console.log('Can\'t use port ' + port);
+const udpPorts = [
+  53, // dns
+  123, // ntp
+];
+
+const mainNodeV = Number(String(process.versions.node).split('.')[0]);
+if (mainNodeV < 18) throw new Error('Node v18 or superior required.');
+
+(() => {
+  console.log(`Starting server on TCP ports: ${tcpPorts.join(',')}`);
+  const pjson = require('./package.json');
+  tcpPorts.forEach((port, index) => {
+    const server = httpServer.createServer((req, res) => {
+      if (req.url === '/version') return res.end(pjson.version);
+      res.end('1');
+    }).listen(port);
+    server.on('error', (err) => {
+      console.log('');
+      console.log(`TCP port ${port} not available.`)
+    });
   });
-});
+
+  udpPorts.forEach((port) => {
+    const dgramServer = dgram.createSocket('udp4');
+    dgramServer.on('error', (err) => {
+      console.log(`server error:\n${err.stack}`);
+    });
+    dgramServer.on('listening', () => {
+      console.log(`Port UDP listening: ${dgramServer.address().port}`);
+    });
+    dgramServer.on('message', (msg, info) => {
+      // console.log('Data received from client: ' + msg.toString());
+      // console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+      const response = Buffer.from('From server: your msg is received');
+      dgramServer.send(response,info.port);
+    });
+    try {
+      dgramServer.bind({
+        port: port,
+        exclusive: false,
+      });
+    } catch (error) {
+      console.log(`UDP port ${port} not available.`)
+    }
+  });
+})();
+
--- test.js
+++ test.js
@@ -1,10 +1,14 @@
+const dgram = require('node:dgram');
 
-const ports = [
+const udpPorts = [
   53, // dns
+  123, // ntp
+];
+
+const tcpPorts = [
   80, // http
   82, // ngrok
   83, // ngrok
-  // 123, // ntp NOT available from node. Alernative check: nc -vz the-ip 123
   443, // https
   1883, // emqtt
   4443, // ngrok tunneling
@@ -14,23 +18,73 @@ const ports = [
   9000, // firmware upgrade
 ];
 
+if (!process.argv[2]) throw new Error('Missing ip. E.g. npm run test 127.0.0.1');
 let theIp = process.argv[2];
+theIp = theIp.replace('http://', ''); // just int case the user has set http:// in the argument.
 
-let c = 0;
-const testOnePort = (port) => {
-  console.log(`Fetching: http://${theIp}:${port}`);
-  fetch(`http://${theIp}:${port}`)
-    .then(() => {
-      // console.log(`Port ${port}: ${response.status}`);
-    })
-    .catch(() => {
-      console.log('Error fetching port ' + port);
-      // throw err;
-    })
-    .finally(() => {
-      c++;
-      if (ports[c])
-        testOnePort(ports[c]);
-    });
-};
-testOnePort(ports[c]);
+const mainNodeV = Number(String(process.versions.node).split('.')[0]);
+if (mainNodeV < 18) throw new Error('Node v18 or superior required.');
+
+(async() => {
+  await fetch(`http://${theIp}/version`)
+  .then(async(response) => {
+    const version = await response.text();
+    console.log(`Diagnose tool server version ${version}`);
+  })
+  .catch(() => {
+    console.log('Error fetching server.');
+  })
+
+  await new Promise((resolve) => {
+    let c = 0;
+    console.log('');
+    const testTcpPort = async (port) => {
+      console.log(`Fetching: http://${theIp}:${port}`);
+      await fetch(`http://${theIp}:${port}`)
+        .then((response) => {
+          console.log(`Port ${port}: ${response.status}`);
+        })
+        .catch(() => {
+          console.log('Error fetching port ' + port);
+          // throw err;
+        })
+        .finally(() => {
+          c++;
+          if (tcpPorts[c])
+            testTcpPort(tcpPorts[c]);
+          else
+            resolve(true);
+        });
+    };
+    testTcpPort(tcpPorts[c]);
+  });
+  console.log('------------');
+
+  await new Promise((resolve) => {
+    let d = 0;
+    const testUdpPort = async (port) => {
+      const udpClient = dgram.createSocket('udp4');
+
+      udpClient.on('message', (msg, info) => {
+        console.log(`Received from server: ${msg.toString()} on port ${port}`);
+      });
+
+      const data = Buffer.from('Hello');
+      udpClient.send(data, port, theIp, (error) => {
+        if (error) console.log(error);
+        console.log('Data sent to udp port ' + port);
+        d++;
+        if (udpPorts[d])
+          testUdpPort(udpPorts[d]);
+        else
+          resolve(true);
+      });
+    };
+    testUdpPort(udpPorts[d]);
+  });
+  console.log('Tests done.')
+
+  setTimeout(() => {
+    process.exit(0);
+  }, 10 * 1000)
+})();
